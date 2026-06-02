23869ea0622e01b21286339eee1fb183e7571218
marcelo-amx <marceloo.amds@gmail.com>
2026-06-02 18:35:53 -0400

    Terminando alterações no filtrada.json e ajustando a lógica de pegar modelos diferentes com o ","

--- app/lib/src/main/java/com/example/lib/Main.kt
+++ app/lib/src/main/java/com/example/lib/Main.kt
@@ -1,6 +1,8 @@
 package com.example.lib
 
-var zeroCount: Int = 0
+import java.text.Normalizer
+
+var carrosEncontrados: Int = 0
 
 fun main() {
     lerCarros()
@@ -10,7 +12,7 @@ private fun lerCarros() {
 
     println("Iniciando processamento...")
     val carrosFipe = LeitorJson.carregarFipe()
-    val carrosFiltrados = LeitorJson.carregarFiltrados()
+    val carrosFiltrados = LeitorJson.carregarFiltrados().sortedBy { it.brand }
 
     println("Fipe: ${carrosFipe.size} registros")
     println("Filtrados: ${carrosFiltrados.size} registros")
@@ -22,30 +24,49 @@ private fun lerCarros() {
             processarCarro(filtrado, fipeMatches)
         }
     }
-    println("Sem nada: $zeroCount")
+    println("Carros encontrados: $carrosEncontrados")
     println("Processamento finalizado.")
 }
 
-private fun processarCarro(filtrado: CarroFiltrado, brancFipeMatches: List<CarroFipe>) {
+private fun processarCarro(filtrado: CarroFiltrado, brandFipeMatches: List<CarroFipe>) {
     val fipeCodes: MutableList<String> = mutableListOf()
-    brancFipeMatches.forEach { fipe ->
-        if (fipe.model.hasAllCharacters(filtrado.model)) {
-            fipeCodes.add(fipe.id)
+
+    if (filtrado.model.contains(",")) {
+        filtrado.model.split(",").forEach {
+            fipeCodes.addAll(brandFipeMatches.pegarCodigosFipe(it, filtrado.yearStart, filtrado.yearEnd))
         }
+    } else {
+        fipeCodes.addAll(brandFipeMatches.pegarCodigosFipe(filtrado.model,filtrado.yearStart, filtrado.yearEnd))
     }
 
     filtrado.fipeCodes = fipeCodes
-    if (fipeCodes.isEmpty()) {
+    if (fipeCodes.isNotEmpty()) {
         println("Encontrado ${fipeCodes.size} códigos para ${filtrado.brand}      ${filtrado.model}")
-        if(!filtrado.model.contains(",")) zeroCount++
+        carrosEncontrados++
     }
 }
 
+private fun List<CarroFipe>.pegarCodigosFipe(filtradoModelo: String, anoInicio: String, anoFim: String) : List<String> {
+    val fipeCodes: MutableList<String> = mutableListOf()
+
+    forEach { fipe ->
+        if (fipe.model.hasAllCharacters(filtradoModelo)) {
+            if(!fipeCodes.contains(fipe.id)) fipeCodes.add(fipe.id)
+        }
+    }
+    return fipeCodes
+}
+
 fun String.hasAllCharacters(needed: String): Boolean {
-    val containerCounts = lowercase().groupingBy { it }.eachCount()
-    val neededCounts = needed.lowercase().groupingBy { it }.eachCount()
+    val containerCounts = lowercase().removeAccents().groupingBy { it }.eachCount()
+    val neededCounts = needed.lowercase().removeAccents().groupingBy { it }.eachCount()
 
     return neededCounts.all { (char, count) ->
         containerCounts.getOrDefault(char, 0) >= count
     }
+}
+
+fun String.removeAccents(): String {
+    val decomposed = Normalizer.normalize(this, Normalizer.Form.NFD)
+    return decomposed.replace("\\p{InCombiningDiacriticalMarks}+".toRegex(), "")
 }
